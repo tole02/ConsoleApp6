cacc68866669501a450f3fa628fe29523494e4bc
tole02 <60465679+tole02@users.noreply.github.com>
2020-01-30 10:51:37 +0100

    start

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -10,6 +10,9 @@ namespace ConsoleApp6
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hej");
+            
+            Console.ReadLine();
         }
     }
 }
